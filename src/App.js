2be65585f47e82955b8a277c7c1fad7845624dfa
Vũ Tiến Đức <107258145+TanPhatStore@users.noreply.github.com>
2023-06-12 16:44:01 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -2,27 +2,16 @@ import './App.css';
 import Header from './Header';
 import Content from './Content';
 import Login from './Login';
-import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 function App() {
-  
-  const [users, setUsers] = useState([])
-  const [reload,  setReload] =  useState(false)
-  useEffect (() => {
-      fetch('https://todo-nodejs-psi.vercel.app/api/users')
-          .then(res => res.json())
-          .then(data => setUsers(data))
-  },[reload])
-
-  const user = JSON.parse(localStorage.getItem('user'))
-
   return (
     <Router>
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<Login reload={reload} setReload={setReload} />} />
-          {(user != null && user.accessControl  == true) ? <Route path= '/home' element={<Content />} /> : <></>}
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Content />} />
         </Routes>
       </div>
     </Router>
